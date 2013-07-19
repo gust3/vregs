ca5b74c7366cf79ac387444ff87f358ccf2c075f
«Владимир <gust3@mail.ru>
2013-07-19 19:42:53 +0400

    obzor fix

--- administrator/components/com_gustvmtools/views/opisaniya/tmpl/default_body.php
+++ administrator/components/com_gustvmtools/views/opisaniya/tmpl/default_body.php
@@ -14,7 +14,7 @@ foreach ($this->items as $i => $item):
             <a href="<?php echo JRoute::_('index.php?option=com_gustvmtools&task=save&view=opisanie&id='.$item->id); ?>"><?php echo $item->product_id; ?></a>
         </td>
 		<td>
-            <a href="<?php echo JRoute::_('index.php?option=com_gustvmtools&task=save&view=opisanie&id='.$item->id); ?>"><?php echo $item->text; ?></a>
+            <a href="<?php echo JRoute::_('index.php?option=com_gustvmtools&task=save&view=opisanie&id='.$item->id); ?>"><?php echo $item->review; ?></a>
         </td>
 		<td align="center">
            <?php echo JHtml::_('jgrid.published', $item->published, $i, '', true, 'cb', null, null); ?>
